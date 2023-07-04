412e06c1c168ca1ba538bde7bc39efb825ab7527
Vadym Kolomiiets <maestrovadkol@gmail.com>
2023-07-04 15:18:59 +0300

    Minor-fix in router (prettier)

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import {  NotFoundPage } from '../components/NotFoundPage';
+import { NotFoundPage } from '../components/NotFoundPage';
 import { CarsTablePage, Contacts } from '../pages';
 
 export const PageRouter: React.FC = () => {
   return (
     <Routes>
-    <Route path="Cars">
-      <Route index element={<CarsTablePage />} />
-    </Route>
+      <Route path="Cars">
+        <Route index element={<CarsTablePage />} />
+      </Route>
 
-    <Route path="Contacts" element={<Contacts />} />
+      <Route path="Contacts" element={<Contacts />} />
 
-    <Route path="/" element={<Navigate to="Cars" replace />} />
+      <Route path="/" element={<Navigate to="Cars" replace />} />
 
-    <Route path="*" element={<NotFoundPage />} />
-  </Routes>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 };
